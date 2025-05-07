a38b71193634ce54fd1a043ed2c257a7ed31167f
Constantin Gavrila <Constantin.Gavrila@citywonders.com>
2025-05-07 22:07:16 +0100

    main layout AOT implementation fix

---
+++ MessageFlow.Client/Pages/Shared/Layout/MainLayout.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MessageFlow.Client.Pages.Shared.Layout
+{
+    public partial class MainLayout : LayoutComponentBase
+    {
+        public MainLayout() { }
+    }
+}
